f64e4c5c5383b523dbb58b39ef6677e0cb4c3701
stamas01 <stamas02@gmail.com>
2020-09-04 14:26:50 +0100

    Updates setup files

--- setup.py
+++ setup.py
@@ -8,10 +8,24 @@ setup(
    version='1.0',
    description='A module to easily read frames from from a vide and display useful video informations.',
    license="MIT",
+   keywords = ['vide', 'frame', 'frame-reader'],
    long_description=long_description,
    author='Tamás Süveges',
+   url = 'https://github.com/stamas02/frame_generator',
    author_email='stamas01@gmail.com',
    packages=['frame_generator'],
    install_requires=['opencv-python', 'tabulate'],
-
+   classifiers=[
+      'Development Status :: 4 - Beta',
+      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+      'Intended Audience :: Developers',  # Define that your audience are developers
+      'Topic :: Software Development :: Build Tools',
+      'License :: OSI Approved :: MIT License',  # Again, pick a license
+      'Programming Language :: Python :: 3',  # Specify which pyhton versions that you want to support
+      'Programming Language :: Python :: 3.4',
+      'Programming Language :: Python :: 3.5',
+      'Programming Language :: Python :: 3.6',
+      'Programming Language :: Python :: 3.7',
+      'Programming Language :: Python :: 3.8',
+   ],
 )
